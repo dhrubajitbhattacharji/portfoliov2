30b52d8c352459950db1f2f1959d18f9fc42f4b8
dhrubajitbhattacharji <dhrubajitbhattacharji@gmail.com>
2024-01-25 22:08:26 +0530

    deploy

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center">
       <p className="text-ls">
-         &copy; Dhrubajit Bhattacharya, 2024. Made with ❤️ using NextJS 14 and Framer Motion. Thank you for visiting.</p>
+         &copy; Dhrubajit Bhattacharya, 2024. Made with ❤️ using NextJS 14 and Framer Motion. Thank you for visiting.....</p>
     </footer>
   );
 }
